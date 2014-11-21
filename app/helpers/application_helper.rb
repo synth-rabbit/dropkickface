514b6d7938b640c1922e704fd9a5d19dd1431e91
Joshua Edwards <josh.dean.edwards@gmail.com>
2014-11-20 16:07:38 -0800

    added back helpers, comics routes, comics views for comic partial, home, and show

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,21 @@
 module ApplicationHelper
+  def next_comic(comic)
+    if comic != Comic.last
+      comic.id + 1
+    else
+      comic.id
+    end
+  end
 
+  def previous_comic(comic)
+    if comic != Comic.first
+      comic.id - 1
+    else
+      comic.id
+    end
+  end
+
+  def random_comic
+    1 + rand(Comic.count)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  root to: "pages#about"
+  root to: "comics#home", id: Comic.last.id
   get 'about', to: 'pages#about'
   get 'blog', to: 'posts#index'
   resources :posts, only: [:show, :create, :destroy]
+  resources :comics, only: [:index, :show, :create, :destroy]
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
